7f837dca17216acb685ff32954bb0ba6e5bab19f
RossFY <yfang14@stevens.edu>
2019-03-04 01:33:22 -0500

    Update

---
+++ src/Functions.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import math
+import operator
+
+def loadDataset(filename):
+    data = pd.read_csv(filename,
+                       usecols=['AST/TOV', 'STL/TOV', 'Height', 'Weight', 'PTSPM', 'DREBPM', 'ASTPM', 'STLPM', 'BLKPM',
+                                'OREBPM', 'Pos'])
+    data = data.dropna()
+    data.to_csv('Result.csv')
+
+    try:
+        file = open('Result.csv', 'r', encoding="gbk")
+        next(file)
+        context = file.read()
+        list_result = context.split("\n")
+        length = len(list_result)
+        for i in range(length):
+            list_result[i] = list_result[i].split(",")
+        list_result.pop()
+        test_data = []
+        training_data = []
+        for i in range(len(list_result)):
+            if i % 5 == 0:
+                test_data.append(list_result[i])
+            else:
+                training_data.append(list_result[i])
+        return test_data, training_data
+    except Exception :
+        print("Could not read file, please check the file path.")
+    finally:
+        file.close()
+
+
+def getDistance(instance1, instance2, length):
+    distance = 0
+    for x in range(1, length):
+        distance += pow((float(instance1[x]) - float(instance2[x])), 2)
+    return math.sqrt(distance)
+
+def getNeighbors(training_data, test, k):
+    distances = []
+    length = len(test) - 1
+    for x in range(len(training_data)):
+        dist = getDistance(test, training_data[x], length)
+        distances.append((training_data[x], dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbors = []
+    for x in range(k):
+        neighbors.append(distances[x][0])
+    return neighbors
+
+def getResponse(neighbors):
+    classVotes = {}
+    for x in range(len(neighbors)):
+        response = neighbors[x][-1]
+        if response in classVotes:
+            classVotes[response] += 1
+        else:
+            classVotes[response] = 1
+    sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedVotes[0][0]
+
+def getAccuracy(test_data, predictions):
+    correct = 0
+    for x in range(len(test_data)):
+        if test_data[x][-1] == predictions[x]:
+            correct += 1
+    return (correct / float(len(test_data))) * 100.0
---
+++ src/Main.py
@@ -0,0 +1,478 @@
+import Functions
+
+if __name__ == "__main__":
+    filename = 'players_stats.csv'
+    test_data, training_data = Functions.loadDataset(filename)
+
+    print("The program will show different results due to different k values: ")
+    print()
+
+    k = 5
+    print('1. When k is', k)
+    predictions1 = []
+    matched = 0
+    unmatched = 0
+
+    PG_PG = 0
+    PG_SG = 0
+    PG_SF = 0
+    PG_PF = 0
+    PG_C = 0
+
+    SG_PG = 0
+    SG_SG = 0
+    SG_SF = 0
+    SG_PF = 0
+    SG_C = 0
+
+    SF_PG = 0
+    SF_SG = 0
+    SF_SF = 0
+    SF_PF = 0
+    SF_C = 0
+
+    PF_PG = 0
+    PF_SG = 0
+    PF_SF = 0
+    PF_PF = 0
+    PF_C = 0
+
+    C_PG = 0
+    C_SG = 0
+    C_SF = 0
+    C_PF = 0
+    C_C = 0
+
+    for x in range(len(test_data)):
+        neighbors = Functions.getNeighbors(training_data, test_data[x], k)
+        result = Functions.getResponse(neighbors)
+        predictions1.append(result)
+
+        if repr(result) == repr('PG'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                PG_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                PG_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                PG_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                PG_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                PG_C += 1
+        elif repr(result) == repr('SG'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                SG_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                SG_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                SG_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                SG_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                SG_C += 1
+        elif repr(result) == repr('SF'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                SF_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                SF_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                SF_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                SF_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                SF_C += 1
+        elif repr(result) == repr('PF'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                PF_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                PF_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                PF_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                PF_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                PF_C += 1
+        elif repr(result) == repr('C'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                C_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                C_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                C_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                C_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                C_C += 1
+
+        if repr(result) != repr(test_data[x][-1]):
+            unmatched += 1
+        else:
+            matched += 1
+        # print('predicted = ' + repr(result) + ', actual = ' + repr(test_data[x][-1]))
+
+    accuracy = Functions.getAccuracy(test_data, predictions1)
+
+    print("Table: (first row is the predicted values, first column is actual values)")
+    print("\tPG\tSG\tSF\tPF\tC")
+    print("PG\t%d\t%d\t%d\t%d\t%d" % (PG_PG, SG_PG, SF_PG, PF_PG, C_PG))
+    print("SG\t%d\t%d\t%d\t%d\t%d" % (PG_SG, SG_SG, SF_SG, PF_SG, C_SG))
+    print("SF\t%d\t%d\t%d\t%d\t%d" % (PG_SF, SG_SF, SF_SF, PF_SF, C_SF))
+    print("PF\t%d\t%d\t%d\t%d\t%d" % (PG_PF, SG_PF, SF_PF, PF_PF, C_PF))
+    print("C\t%d\t%d\t%d\t%d\t%d" % (PG_C, SG_C, SF_C, PF_C, C_C))
+    print()
+    print('Total test data:', (matched + unmatched))
+    print('Matched:', matched, 'Unmatched:', unmatched)
+    print('Accuracy: ' + repr(accuracy) + '%')
+    print()
+
+    k = 10
+    print('2. When k is', k)
+    predictions2 = []
+    matched = 0
+    unmatched = 0
+
+    PG_PG = 0
+    PG_SG = 0
+    PG_SF = 0
+    PG_PF = 0
+    PG_C = 0
+
+    SG_PG = 0
+    SG_SG = 0
+    SG_SF = 0
+    SG_PF = 0
+    SG_C = 0
+
+    SF_PG = 0
+    SF_SG = 0
+    SF_SF = 0
+    SF_PF = 0
+    SF_C = 0
+
+    PF_PG = 0
+    PF_SG = 0
+    PF_SF = 0
+    PF_PF = 0
+    PF_C = 0
+
+    C_PG = 0
+    C_SG = 0
+    C_SF = 0
+    C_PF = 0
+    C_C = 0
+
+    for x in range(len(test_data)):
+        neighbors = Functions.getNeighbors(training_data, test_data[x], k)
+        result = Functions.getResponse(neighbors)
+        predictions2.append(result)
+
+        if repr(result) == repr('PG'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                PG_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                PG_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                PG_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                PG_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                PG_C += 1
+        elif repr(result) == repr('SG'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                SG_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                SG_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                SG_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                SG_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                SG_C += 1
+        elif repr(result) == repr('SF'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                SF_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                SF_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                SF_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                SF_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                SF_C += 1
+        elif repr(result) == repr('PF'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                PF_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                PF_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                PF_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                PF_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                PF_C += 1
+        elif repr(result) == repr('C'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                C_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                C_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                C_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                C_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                C_C += 1
+
+        if repr(result) != repr(test_data[x][-1]):
+            unmatched += 1
+        else:
+            matched += 1
+        # print('predicted = ' + repr(result) + ', actual = ' + repr(test_data[x][-1]))
+    accuracy = Functions.getAccuracy(test_data, predictions2)
+
+    print("Table: (first row is the predicted values, first column is actual values)")
+    print("\tPG\tSG\tSF\tPF\tC")
+    print("PG\t%d\t%d\t%d\t%d\t%d" % (PG_PG, SG_PG, SF_PG, PF_PG, C_PG))
+    print("SG\t%d\t%d\t%d\t%d\t%d" % (PG_SG, SG_SG, SF_SG, PF_SG, C_SG))
+    print("SF\t%d\t%d\t%d\t%d\t%d" % (PG_SF, SG_SF, SF_SF, PF_SF, C_SF))
+    print("PF\t%d\t%d\t%d\t%d\t%d" % (PG_PF, SG_PF, SF_PF, PF_PF, C_PF))
+    print("C\t%d\t%d\t%d\t%d\t%d" % (PG_C, SG_C, SF_C, PF_C, C_C))
+    print()
+    print('Total test data:', (matched + unmatched))
+    print('Matched:', matched, 'Unmatched:', unmatched)
+    print('Accuracy: ' + repr(accuracy) + '%')
+    print()
+
+    k = 15
+    print('3. When k is', k)
+    predictions3 = []
+    matched = 0
+    unmatched = 0
+
+    PG_PG = 0
+    PG_SG = 0
+    PG_SF = 0
+    PG_PF = 0
+    PG_C = 0
+
+    SG_PG = 0
+    SG_SG = 0
+    SG_SF = 0
+    SG_PF = 0
+    SG_C = 0
+
+    SF_PG = 0
+    SF_SG = 0
+    SF_SF = 0
+    SF_PF = 0
+    SF_C = 0
+
+    PF_PG = 0
+    PF_SG = 0
+    PF_SF = 0
+    PF_PF = 0
+    PF_C = 0
+
+    C_PG = 0
+    C_SG = 0
+    C_SF = 0
+    C_PF = 0
+    C_C = 0
+
+    for x in range(len(test_data)):
+        neighbors = Functions.getNeighbors(training_data, test_data[x], k)
+        result = Functions.getResponse(neighbors)
+        predictions3.append(result)
+
+        if repr(result) == repr('PG'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                PG_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                PG_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                PG_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                PG_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                PG_C += 1
+        elif repr(result) == repr('SG'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                SG_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                SG_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                SG_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                SG_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                SG_C += 1
+        elif repr(result) == repr('SF'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                SF_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                SF_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                SF_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                SF_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                SF_C += 1
+        elif repr(result) == repr('PF'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                PF_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                PF_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                PF_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                PF_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                PF_C += 1
+        elif repr(result) == repr('C'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                C_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                C_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                C_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                C_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                C_C += 1
+
+        if repr(result) != repr(test_data[x][-1]):
+            unmatched += 1
+        else:
+            matched += 1
+        # print('predicted = ' + repr(result) + ', actual = ' + repr(test_data[x][-1]))
+    accuracy = Functions.getAccuracy(test_data, predictions3)
+
+    print("Table: (first row is the predicted values, first column is actual values)")
+    print("\tPG\tSG\tSF\tPF\tC")
+    print("PG\t%d\t%d\t%d\t%d\t%d" % (PG_PG, SG_PG, SF_PG, PF_PG, C_PG))
+    print("SG\t%d\t%d\t%d\t%d\t%d" % (PG_SG, SG_SG, SF_SG, PF_SG, C_SG))
+    print("SF\t%d\t%d\t%d\t%d\t%d" % (PG_SF, SG_SF, SF_SF, PF_SF, C_SF))
+    print("PF\t%d\t%d\t%d\t%d\t%d" % (PG_PF, SG_PF, SF_PF, PF_PF, C_PF))
+    print("C\t%d\t%d\t%d\t%d\t%d" % (PG_C, SG_C, SF_C, PF_C, C_C))
+    print()
+    print('Total test data:', (matched + unmatched))
+    print('Matched:', matched, 'Unmatched:', unmatched)
+    print('Accuracy: ' + repr(accuracy) + '%')
+    print()
+
+
+    k = 20
+    print('4. When k is', k)
+    predictions4 = []
+    matched = 0
+    unmatched = 0
+
+    PG_PG = 0
+    PG_SG = 0
+    PG_SF = 0
+    PG_PF = 0
+    PG_C = 0
+
+    SG_PG = 0
+    SG_SG = 0
+    SG_SF = 0
+    SG_PF = 0
+    SG_C = 0
+
+    SF_PG = 0
+    SF_SG = 0
+    SF_SF = 0
+    SF_PF = 0
+    SF_C = 0
+
+    PF_PG = 0
+    PF_SG = 0
+    PF_SF = 0
+    PF_PF = 0
+    PF_C = 0
+
+    C_PG = 0
+    C_SG = 0
+    C_SF = 0
+    C_PF = 0
+    C_C = 0
+
+    for x in range(len(test_data)):
+        neighbors = Functions.getNeighbors(training_data, test_data[x], k)
+        result = Functions.getResponse(neighbors)
+        predictions4.append(result)
+
+        if repr(result) == repr('PG'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                PG_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                PG_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                PG_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                PG_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                PG_C += 1
+        elif repr(result) == repr('SG'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                SG_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                SG_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                SG_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                SG_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                SG_C += 1
+        elif repr(result) == repr('SF'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                SF_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                SF_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                SF_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                SF_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                SF_C += 1
+        elif repr(result) == repr('PF'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                PF_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                PF_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                PF_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                PF_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                PF_C += 1
+        elif repr(result) == repr('C'):
+            if repr(test_data[x][-1]) == repr('PG'):
+                C_PG += 1
+            elif repr(test_data[x][-1]) == repr('SG'):
+                C_SG += 1
+            elif repr(test_data[x][-1]) == repr('SF'):
+                C_SF += 1
+            elif repr(test_data[x][-1]) == repr('PF'):
+                C_PF += 1
+            elif repr(test_data[x][-1]) == repr('C'):
+                C_C += 1
+
+        if repr(result) != repr(test_data[x][-1]):
+            unmatched += 1
+        else:
+            matched += 1
+        # print('predicted = ' + repr(result) + ', actual = ' + repr(test_data[x][-1]))
+    accuracy = Functions.getAccuracy(test_data, predictions4)
+
+    print("Table: (first row is the predicted values, first column is actual values)")
+    print("\tPG\tSG\tSF\tPF\tC")
+    print("PG\t%d\t%d\t%d\t%d\t%d" % (PG_PG, SG_PG, SF_PG, PF_PG, C_PG))
+    print("SG\t%d\t%d\t%d\t%d\t%d" % (PG_SG, SG_SG, SF_SG, PF_SG, C_SG))
+    print("SF\t%d\t%d\t%d\t%d\t%d" % (PG_SF, SG_SF, SF_SF, PF_SF, C_SF))
+    print("PF\t%d\t%d\t%d\t%d\t%d" % (PG_PF, SG_PF, SF_PF, PF_PF, C_PF))
+    print("C\t%d\t%d\t%d\t%d\t%d" % (PG_C, SG_C, SF_C, PF_C, C_C))
+    print()
+    print('Total test data:', (matched + unmatched))
+    print('Matched:', matched, 'Unmatched:', unmatched)
+    print('Accuracy: ' + repr(accuracy) + '%')
+
